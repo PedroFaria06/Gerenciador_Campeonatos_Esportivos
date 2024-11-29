9b96f0d9d61e085a502bde14ae72cf8b949220d2
Pedro Gabriel <pedrobuenofaria16@gmail.com>
2024-11-29 19:35:15 -0300

    Tela de Login Finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';  
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,  
+  imports: [LoginComponent]  
 })
 export class AppComponent {
-  title = 'Gerenciador_Esportivo';
+  title = 'Gerenciador Esportivo';
 }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  standalone: true,  
+  imports: [FormsModule]  
+})
+export class LoginComponent {
+  email: string = '';  
+  password: string = '';  
+
+
+  onSubmit() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+
+
+    if (this.email === 'usuario@exemplo.com' && this.password === 'senha123') {
+      console.log('Login bem-sucedido!');
+    } else {
+      console.log('Credenciais inválidas');
+    }
+  }
+}
