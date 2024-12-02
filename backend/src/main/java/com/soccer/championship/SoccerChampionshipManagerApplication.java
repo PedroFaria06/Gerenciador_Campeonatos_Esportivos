9de5159d4b94d87768dc2854c22f6af2d25cf352
lucasbassetto <lucasfbassetto@gmail.com>
2024-12-02 16:48:47 -0300

    feat: first commit

---
+++ backend/src/main/java/com/soccer/championship/SoccerChampionshipManagerApplication.java
@@ -0,0 +1,13 @@
+package com.soccer.championship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerChampionshipManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerChampionshipManagerApplication.class, args);
+	}
+
+}
