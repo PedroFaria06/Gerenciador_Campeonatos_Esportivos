ec6a536f23a6956263a8e5f1f702e1c970158bd8
Joaostopa <joao.vitorstopa@hotmail.com>
2025-01-15 18:11:15 -0300

    Commit tela dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from './login/login.component';  
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  standalone: true,  
-  imports: [LoginComponent]  
 })
 export class AppComponent {
   title = 'Gerenciador Esportivo';
