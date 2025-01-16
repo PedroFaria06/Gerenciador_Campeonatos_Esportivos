afea10935c205cc0c5282c31e551209eebe47a81
Joaostopa <joao.vitorstopa@hotmail.com>
2025-01-15 21:33:47 -0300

    commit arrumando login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-
+import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-@NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    CommonModule, 
-    LoginComponent,
-    CadastroComponent,
-    DashboardComponent,
-  ],
-  providers: [],
-})
-export class AppModule { }
+
