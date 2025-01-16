eb63d8740bcdd8cd104cb48c955552259a977fcb
joaoscalice <jv.scalice@gmail.com>
2025-01-15 22:33:45 -0300

    Criação componente 'relatorio' e inclusão de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './login/login.component'
+import { CadastroComponent } from './cadastro/cadastro.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { RelatorioComponent } from './relatorio/relatorio.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'relatorio', component: RelatorioComponent },
 ];
---
+++ src/app/relatorio/relatorio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relatorio',
+  imports: [],
+  templateUrl: './relatorio.component.html',
+  styleUrl: './relatorio.component.css'
+})
+export class RelatorioComponent {
+
+}
