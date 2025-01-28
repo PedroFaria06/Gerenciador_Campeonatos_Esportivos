02c8f5400dd3ef7fbc8f082c8cfb81bbacfc45f4
Joaostopa <joao.vitorstopa@hotmail.com>
2025-01-28 17:29:56 -0300

    unificando branch

--- backend/src/main/java/com/soccer/championship/config/CorsConfiguration.java
+++ backend/src/main/java/com/soccer/championship/config/CorsConfiguration.java
@@ -2,7 +2,6 @@ package com.soccer.championship.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,11 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-<<<<<<< HEAD
       .allowedOrigins("http://localhost")
-=======
-      .allowedOrigins("http://localhost:4200")
->>>>>>> b9ec6f76c096b3f0e0070124735fe998be0bfd9b
       .allowedMethods("*");
   }
 
