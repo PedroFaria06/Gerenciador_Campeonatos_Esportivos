c33ee626f3697e2a121e2c42655f3b18fa17bfe5
PEDRO GABRIEL <114080858+PedroFaria06@users.noreply.github.com>
2025-02-06 22:18:03 +0000

    feat: add championshipTeamDto  interface

---
+++ src/app/models/championship-team.interface.ts
@@ -0,0 +1,14 @@
+export interface ChampionshipTeamDTO {
+    championshipId: number;
+    teamId: number;
+    teamName: string;
+    points: number;
+    matchesPlayed: number;
+    victories: number;
+    draws: number;
+    defeats: number;
+    goalsFor: number;
+    goalsAgainst: number;
+    goalDifference: number;
+  }
+  
