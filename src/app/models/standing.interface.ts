cd4da0addc4e1cd148dbd8339f64cac5ce2c35f0
tamiuemura13 <163928004+tamiuemura13@users.noreply.github.com>
2025-02-06 22:57:41 +0000

    feat: add standinng models

---
+++ src/app/models/standing.interface.ts
@@ -0,0 +1,13 @@
+export interface Standing {
+    teamId: number;
+    teamName: string;
+    points: number;
+    matches: number;
+    victories: number;
+    draws: number;
+    defeats: number;
+    goalsFor: number;
+    goalsAgainst: number;
+    goalDifference: number;
+  }
+  
