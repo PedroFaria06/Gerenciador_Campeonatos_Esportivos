9034d994d560f83b930e1ab61e539b7c61784b07
tamiuemura13 <163928004+tamiuemura13@users.noreply.github.com>
2025-02-06 22:57:09 +0000

    feat: add playerMatch models

---
+++ src/app/models/player-match.interface.ts
@@ -0,0 +1,9 @@
+export interface PlayerMatch {
+    playerId: number;
+    matchId: number;
+    shirtNumber: number;
+    isStarter: boolean;
+    position: string;
+    playerName: string;
+  }
+  
