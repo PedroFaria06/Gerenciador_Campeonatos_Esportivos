68afc6429d8464e9dba8f03403cfae3dff2e5894
Joaostopa <120514903+Joaostopa@users.noreply.github.com>
2025-02-12 16:06:44 +0000

    refactor: type match-event

--- src/app/models/match-event.interface.ts
+++ src/app/models/match-event.interface.ts
@@ -1,11 +1,10 @@
 export interface MatchEvent {
-    id?: number;
-    matchId: number;
-    eventType: EventType;
-    playerId: number;
-    minute: number;
-    observation?: string;
-  }
-  
-  export type EventType = 'GOAL' | 'YELLOW_CARD' | 'RED_CARD' | 'SUBSTITUTION';
-  
+  id?: number;
+  matchId: number;
+  eventType: EventType;
+  playerId: number;
+  eventMinute: number;
+  observation?: string;
+}
+
+export type EventType = 'GOAL' | 'YELLOW_CARD' | 'RED_CARD' | 'SUBSTITUTION';
